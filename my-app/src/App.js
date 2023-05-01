9f2f478352f0caaad9138e8dfdd41cc7ae1d2884
Lucas Borella <blackbox.software@gmail.com>
2023-05-01 10:48:00 -0300

    Quick start

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import Login from "./login/login.view";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
